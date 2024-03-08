acf8d6dfb0e14270d689cea763cc8f562dafa57b
Ishaqezaz <78658114+Ishaqezaz@users.noreply.github.com>
2024-03-08 14:55:00 +0100

    feat #11: Modified ExportRowsCommand to accommodate StreamExporter2  (#15)
    * feat #9: Create new exportRows
    
    * feat #11: Modified ExportRowsCommand to accommodate StreamExporter2
    
    * fix #11: Changed variable name from paramsMap to params.
    
    ---------
    
    Co-authored-by: Edvin Nordling <brollelir@gmail.com>

--- main/src/com/google/refine/commands/project/ExportRowsCommand.java
+++ main/src/com/google/refine/commands/project/ExportRowsCommand.java
@@ -38,6 +38,8 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.servlet.ServletException;
@@ -56,7 +58,9 @@ import com.google.refine.exporters.CsvExporter;
 import com.google.refine.exporters.Exporter;
 import com.google.refine.exporters.ExporterRegistry;
 import com.google.refine.exporters.StreamExporter;
+import com.google.refine.exporters.StreamExporter2;
 import com.google.refine.exporters.WriterExporter;
+import com.google.refine.exporters.WriterExporter2;
 import com.google.refine.exporters.sql.SqlExporterException;
 import com.google.refine.model.Project;
 
@@ -80,6 +84,18 @@ public class ExportRowsCommand extends Command {
         return options;
     }
 
+    public static Map<String, String> getRequestParameters2(HttpServletRequest request) {
+        Map<String, String> options = new HashMap<>();
+
+        Enumeration<String> en = request.getParameterNames();
+        while (en.hasMoreElements()) {
+            String name = en.nextElement();
+            String value = request.getParameter(name);
+            options.put(name, value);
+        }
+        return options;
+    }
+
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -89,21 +105,22 @@ public class ExportRowsCommand extends Command {
         try {
             Project project = getProject(request);
             Engine engine = getEngine(request, project);
-            Properties params = getRequestParameters(request);
+            Map<String, String> params = getRequestParameters2(request);
 
-            String format = params.getProperty("format");
+
+            String format = params.get("format");
             Exporter exporter = ExporterRegistry.getExporter(format);
             if (exporter == null) {
                 exporter = new CsvExporter('\t');
             }
 
-            String contentType = params.getProperty("contentType");
+            String contentType = params.get("contentType");
             if (contentType == null) {
                 contentType = exporter.getContentType();
             }
             response.setHeader("Content-Type", contentType);
 
-            String preview = params.getProperty("preview");
+            String preview = params.get("preview");
             if (!"true".equals(preview)) {
                 String path = request.getPathInfo();
                 String filename = path.substring(path.lastIndexOf('/') + 1);
@@ -112,27 +129,7 @@ public class ExportRowsCommand extends Command {
                 response.setHeader("Content-Disposition", "attachment; filename=" + filename + "; filename*=utf-8' '" + filename);
             }
 
-            if (exporter instanceof WriterExporter) {
-                String encoding = params.getProperty("encoding");
-
-                response.setCharacterEncoding(encoding != null ? encoding : "UTF-8");
-                Writer writer = encoding == null ? response.getWriter() : new OutputStreamWriter(response.getOutputStream(), encoding);
-
-                ((WriterExporter) exporter).export(project, params, engine, writer);
-                writer.close();
-            } else if (exporter instanceof StreamExporter) {
-                response.setCharacterEncoding("UTF-8");
-
-                OutputStream stream = response.getOutputStream();
-                ((StreamExporter) exporter).export(project, params, engine, stream);
-                stream.close();
-//          } else if (exporter instanceof UrlExporter) {
-//              ((UrlExporter) exporter).export(project, options, engine);
-
-            } else {
-                // TODO: Should this use ServletException instead of respondException?
-                respondException(response, new RuntimeException("Unknown exporter type"));
-            }
+            handleExport(response, exporter, params, project, engine);
         } catch (Exception e) {
             // Use generic error handling rather than our JSON handling
             logger.info("error:{}", e.getMessage());
@@ -142,6 +139,30 @@ public class ExportRowsCommand extends Command {
             throw new ServletException(e);
         } finally {
             ProjectManager.singleton.setBusy(false);
+        }
+    }
+
+    private static void handleExport(HttpServletResponse response, Exporter exporter, Map<String,String> params, Project project, Engine engine) throws IOException, ServletException {
+        if (exporter instanceof WriterExporter) {
+            String encoding = params.get("encoding");
+
+            response.setCharacterEncoding(encoding != null ? encoding : "UTF-8");
+            Writer writer = encoding == null ? response.getWriter() : new OutputStreamWriter(response.getOutputStream(), encoding);
+
+            ((WriterExporter2) exporter).export(project, params, engine, writer);
+            writer.close();
+        } else if (exporter instanceof StreamExporter) {
+            response.setCharacterEncoding("UTF-8");
+
+            OutputStream stream = response.getOutputStream();
+            ((StreamExporter2) exporter).export(project, params, engine, stream);
+            stream.close();
+//          } else if (exporter instanceof UrlExporter) {
+//              ((UrlExporter) exporter).export(project, options, engine);
+
+        } else {
+            // TODO: Should this use ServletException instead of respondException?
+            respondException(response, new RuntimeException("Unknown exporter type"));
         }
     }
 }

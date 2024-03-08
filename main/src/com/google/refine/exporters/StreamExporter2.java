21e95ab36b7c9ed5b5313c1b15b98ec7f1955fc6
Ishaqezaz <78658114+Ishaqezaz@users.noreply.github.com>
2024-03-08 09:58:28 +0100

    Update StreamExporter2.java
    Export parameter changed to OutputStream instead of Writer

--- main/src/com/google/refine/exporters/StreamExporter2.java
+++ main/src/com/google/refine/exporters/StreamExporter2.java
@@ -9,5 +9,5 @@ import com.google.refine.model.Project;
 
 public interface StreamExporter2 extends StreamExporter{
 
-    public void export(Project project, Map<String,String> options, Engine engine, Writer writer) throws IOException;
+    public void export(Project project, Map<String,String> options, Engine engine, OutputStream outputStream) throws IOException;
 }
